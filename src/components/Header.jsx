fb70aa138010978096f1d39470dbee028f63569d
Patrick Herbst <p.herbst.1987@gmail.com>
2024-02-16 11:15:13 +0100

    Create README.md

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header id="header">
+        <h1>React Investment Calculator</h1>
+        <img src="investment-calculator-logo.png" alt="investment calculator logo" />
+        </header>
+    )
+}
